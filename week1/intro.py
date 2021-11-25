ce774e79643f0301026968459340770e8423bdbc
Swetha Vijaya Raju <swethavraj96@gmail.com>
2021-11-25 14:01:02 -0800

    week 1 intro programming

---
+++ week1/intro.py
@@ -0,0 +1,26 @@
+import random as r
+import math as m
+'''
+print("Hello, World")
+print(5)
+print(int(2.8))
+print(int(-2.8))
+print(int(-3 / 2))
+print(int(22 / 7))
+print(22 / 7)
+print(float('inf') > 9)
+print(int(7 / 2) == 7 // 2)
+print(6 + 7 % 5)
+print("Today’s numbers are " + str(1) + ", " + str(1) + ", " + str(1) + ", " + str(1) + " and " + str(
+    1) + ". The Powerball number is " + str(1) + ".")
+a = random.randrange(1, 20)
+print(type(str(a)))
+random.randrange(1, 20)
+print (6--+6)
+print (-1%5)
+print (type(float('inf')))
+'''
+
+print(126 // 60)
+print(7 / 2)
+print(7 % 2)
